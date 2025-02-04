e381e7bff40cc359b08e1a914652c93d6e165a47
Sam Bencivengo <sambencivengo@gmail.com>
2025-02-04 10:17:10 -0500

    init update

--- init.lua
+++ init.lua
@@ -300,18 +300,6 @@ require('lazy').setup({
   --    require('gitsigns').setup({ ... })
   --
   -- See `:help gitsigns` to understand what the configuration keys do
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
@@ -978,7 +966,7 @@ require('lazy').setup({
   require 'kickstart.plugins.colorscheme',
   require 'kickstart.plugins.harpoon',
   require 'kickstart.plugins.rainbow-delimiters',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
